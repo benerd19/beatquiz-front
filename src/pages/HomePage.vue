db96d340ba9d14b40848ab6e41e32f6bc90abe2b
Daniil <dani2003il@mail.ru>
2026-04-18 09:18:59 +0700

    chore: project init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+    <div class="home">
+        <h1 class="home__header">Home</h1>
+    </div>
+</template>
+<style scoped lang="scss">
+.home {
+    &__header {
+        text-align: center;
+        font-size: 50px;
+    }
+}
+</style>
